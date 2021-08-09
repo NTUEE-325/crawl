1b5c730335c12cf820965fd2f319c74e1e21b0e3
tim901231 <b09901020@ntu.edu.tw>
2021-08-09 20:21:13 +0800

    update

--- test1.py
+++ test1.py
@@ -1,33 +1,56 @@
 import requests
 from bs4 import BeautifulSoup
-my_data = {
-    '_csrf': "a498de51-bbe2-48bb-a9fb-7c6bbef854db",
-    'trainTypeList': 'ALL',
-    'transfer': 'ONE',
-    'startStation': '1000-臺北',
-    'endStation': '4400-高雄',
-    'rideDate': '2021/08/09',
-    'startOrEndTime': 'true',
-    'startTime': '00:00',
-    'endTime': '23:59'
-}
-r = requests.post(
-    'https://www.railway.gov.tw/tra-tip-web/tip/tip001/tip112/querybytime', data=my_data)
+from datetime import date, timedelta
 
-soup = BeautifulSoup(r.text, 'html.parser')
-x = soup.find("tr", class_="trip-column")
-# print(x)
-y = x.find_all("td")
 
-print(y)
-t1 = y[1].text
-t2 = y[2].text
-print(t1)
-print(t2)
-print()
-"""
-x = soup.find("td", class_="coloum1").text
-y = soup.find("td", class_="coloum3").text
-z = soup.find("td", class_="coloum4").text
-print(x+''+y+''+z)
-"""
+class My_train:
+    def __init__(self, num, st, et, td, path):
+        self.num = num
+        self.st = st
+        self.et = et
+        self.td = td
+        self.path = path
+
+
+start_month, start_day = map(int, input(
+    "Enter the start date (MM/DD): ").split('/'))
+end_month, end_day = map(int, input(
+    "Enter the start date (MM/DD): ").split('/'))
+d1 = date(2021, start_month, start_day)
+d2 = date(2021, end_month, end_day)
+delta = d2-d1
+
+datas = []
+
+for i in range(delta.days+1):
+    day = d1+timedelta(days=i)
+    day = str(day)
+    days = day.split('-')
+    day = '/'.join(days)
+    my_data = {
+        '_csrf': "a498de51-bbe2-48bb-a9fb-7c6bbef854db",
+        'trainTypeList': 'ALL',
+        'transfer': 'ONE',
+        'startStation': '1000-臺北',
+        'endStation': '4400-高雄',
+        'rideDate': day,
+        'startOrEndTime': 'true',
+        'startTime': '00:00',
+        'endTime': '23:59'
+    }
+    r = requests.post(
+        'https://www.railway.gov.tw/tra-tip-web/tip/tip001/tip112/querybytime', data=my_data)
+
+    soup = BeautifulSoup(r.text, 'html.parser')
+    trains = soup.find_all("tr", class_="trip-column")
+    for train in trains:
+        r = train.find_all("td")
+        num = r[0].find("a").text
+        t1 = r[1].text
+        t2 = r[2].text
+        td = r[3].text
+        path = r[4].txt
+        datas.append(My_train(num, t1, t2, td, path))
+
+for data in datas:
+    print(data.num)
